1a1197d85a762de48317188b448f5ba377d12ea2
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-05-09 18:51:26 -0700

    finished laying the app foundation

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Button from '@/components/ui/Button'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button>hello</Button>
+}
+
+export default page
